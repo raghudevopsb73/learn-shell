bf7075a5da3fa13d8b75305e8e623d6118bbc360
Raghu K <raghuk.vit@gmail.com>
2023-05-29 08:42:06 +0530

    Payment

--- 03-vars.sh
+++ 03-vars.sh
@@ -4,3 +4,11 @@ echo Value of a is $a dollars
 
 # Alternatively variables also can be accessed by using ${a}
 echo Value of a is ${a}dollars
+
+
+# Special variables for inputs
+# Special variables used for inputs are $0 to $n, $* , $#
+echo $0
+echo $1
+echo $*
+echo $#
