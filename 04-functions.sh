7b39e6bebcbb9865a8f6625f95836e664f0c2c81
Raghu K <raghuk.vit@gmail.com>
2023-05-29 08:47:39 +0530

    Payment

--- 04-functions.sh
+++ 04-functions.sh
@@ -3,3 +3,12 @@ print_hello() {
 }
 
 print_hello
+
+print_arguments() {
+  echo First Argument $1
+  echo Second Argument $2
+  echo All Arguments - $*
+  echo No of Argumetns - $#
+}
+
+print_arguments abc 123 xyz
