98f5a4f5b0867f44b509ce88b07d28504f551c0c
Raghu K <raghuk.vit@gmail.com>
2023-05-30 08:56:45 +0530

    mediawiki

--- mediawiki/apache.sh
+++ mediawiki/apache.sh
@@ -1,5 +1,9 @@
 cd /tmp
-curl -O https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz
+url="https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz"
+curl -O $url
+folder=$(echo $url  | awk -F / '{print $6}' | sed 's/.tar.gz//')
 yum install httpd -y
+rm -rf /var/www/html/*
 cd /var/www/html
 tar -xf /tmp/mediawiki-1.39.3.tar.gz
+mv $folder mediawiki
